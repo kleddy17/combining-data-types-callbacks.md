38485b1e98a1195c8fd6140b4cb229775f34194c
kleddy17 <kleddy17@gmail.com>
2020-09-13 14:10:33 -0400

    First-Part-Deliverable

---
+++ programming_fundamentals.js
@@ -0,0 +1,22 @@
+// Write a ~1 sentence summary for each one
+// Which ones surprise you (if any)?
+// Which one is currently giving you the most struggle?
+
+// DRY
+// *  Don't Repeat Yourself, no repititon, once that happens, start a 
+// *  Not surprising, found myself in trouble with constantly repeating myself
+// KISS
+// * Keep is simple, stupid, meaning dont add unessarcy code if there is a simpler
+//  less messy way to write it.
+// * Not surprising
+// Avoid creating a YAGNI
+// * Avoid writing functions that you won't need by creating steps through
+//your comments. 
+// *Not surprising
+// Do the simplest thing that could possibly work
+// * 
+// Don't make me think
+// Write code for the maintainer
+// Single responsibility principle
+// Avoid premature optimization
+// Separation of concerns
